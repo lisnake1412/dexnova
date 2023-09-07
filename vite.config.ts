b5000ee01be13d37b2828968a82ad145a8285af3
cryptolover9x <trancongtaicrypto@gmail.com>
2023-09-07 18:35:13 +0000

    add some info
    add some info

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react' //removed swc
+
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
